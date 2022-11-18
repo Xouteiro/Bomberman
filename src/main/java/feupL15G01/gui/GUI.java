a67fc6ae316ebfd973bfcfbe0e21efe6c60f1f9a
Xouteiro <up202108895@up.pt>
2022-11-18 18:00:59 +0000

    created GUI interface and LanternaGUI

---
+++ src/main/java/feupL15G01/gui/GUI.java
@@ -0,0 +1,30 @@
+package feupL15G01.gui;
+
+import javax.swing.text.Position;
+import java.io.IOException;
+
+public interface GUI {
+    ACTION getNextAction() throws IOException;
+
+    void drawPlayer(Position position);
+
+    void drawWall(Position position);
+
+    void drawMonster(Position position);
+
+    void drawFixedBlock(Position position);
+
+    void drawTempBlocks(Position position);
+
+    void drawPowers(Position position /* , image */ );
+
+    void drawText(Position position, String text, String color);
+
+    void clear();
+
+    void refresh() throws IOException;
+
+    void close() throws IOException;
+
+    enum ACTION {UP, RIGHT, DOWN, LEFT, NONE, QUIT, SELECT}
+}
