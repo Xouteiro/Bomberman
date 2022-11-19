a2a46c07a9957800b9014d433f545e6967c4c6d6
Xouteiro <up202108895@up.pt>
2022-11-19 13:07:47 +0000

    Created the principal Game class Game

---
+++ src/main/java/feupL15G01/Game.java
@@ -0,0 +1,50 @@
+package feupL15G01;
+
+
+import feupL15G01.gui.LanternaGUI;
+import feupL15G01.model.menu.Menu;
+import feupL15G01.states.MenuState;
+import feupL15G01.states.State;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Game {
+    private final LanternaGUI gui;
+    private State state;
+
+    public Game() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGUI(20, 20);
+        this.state = new MenuState(new Menu());
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
+        new Game().start();
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    private void start() throws IOException {
+        int FPS = 10;
+        int frameTime = 1000 / FPS;
+
+        while (this.state != null) {
+            long startTime = System.currentTimeMillis();
+
+            state.step(this, gui, startTime);
+
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            long sleepTime = frameTime - elapsedTime;
+
+            try {
+                if (sleepTime > 0) Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+            }
+        }
+
+        gui.close();
+    }
+}
