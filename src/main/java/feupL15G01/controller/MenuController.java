95b549a763cd391813d4ec43ca30d7b50b8d7893
Xouteiro <up202108895@up.pt>
2022-12-23 00:25:37 +0000

    fixed small bug

--- src/main/java/feupL15G01/controller/MenuController.java
+++ src/main/java/feupL15G01/controller/MenuController.java
@@ -25,9 +25,8 @@ public class MenuController extends Controller<Menu> {
                 break;
             case SELECT:
                 if (getModel().isSelectedExit()) game.setState(null);
-                if (getModel().isSelectedStart()) {
-                    game.setState(new GameState(board));
-                }
+                if (getModel().isSelectedStart())
+                    game.setState(new GameState(new RandomBoardBuilder(32, 32, 4,150 , 15).createBoard()));
 
         }
     }
